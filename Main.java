7b3d3d0fe3e22124ecb2d2baaa7b3cb336bfedcf
LukeTx <35909033+LukeTx@users.noreply.github.com>
2018-11-14 23:35:43 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.blackducksoftware.test;
+
+import java.util.Arrays;
+
+/**
+ * Demo class showing compilation of repository source
+ *
+ * @author LukeTan
+ * @since 0.1
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Arguments! (" + Arrays.toString(args) + ")");
+    }
+
+}
